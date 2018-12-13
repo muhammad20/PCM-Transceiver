f77ac770317de304c84f67cdf80357ec9b184423
Moaz <moaz_elshorbagy@hotmail.com>
2018-12-13 13:19:00 -0800

    Encoder added

---
+++ Encoder.m
@@ -0,0 +1,59 @@
+function [y] = Encoder(type, m, mp, l)
+
+% converting voltage levels to decimals
+for i = 1:1:length(m)
+    output(i)=((m(i)+mp)*l)/(2*mp);
+end
+
+numberOfBits = ceil(log2(l)) + 1;
+bin2dArray = zeros(length(m),numberOfBits);
+
+% converting decimals to binary numbers
+for i = 1:1:length(m)
+    binaryNumber = dec2bin(output(i), numberOfBits);
+    for j = numberOfBits:-1:1
+        bin2dArray(i,j) = binaryNumber(j);
+        bin2dArray(i,j) = bin2dArray(i,j) - 48; 
+    end
+end
+
+% converting binary numbers to voltage levels (A = 5 volt)
+A = 5;
+counter = 1;
+% for each bit in the message
+for i = 1:1:length(m)
+    for j = 1:1:numberOfBits
+        switch type
+            % Unipolar NRZ signaling
+            case 1
+                if bin2dArray(i,j) == 1
+                    final_values(counter) = A;
+                elseif bin2dArray(i,j) == 0
+                    final_values(counter) = 0;
+                end
+            % Polar NRZ signaling
+            case 2
+                if bin2dArray(i,j) == 1
+                    final_values(counter) = A;
+                elseif bin2dArray(i,j) == 0
+                    final_values(counter) = -1*A;
+                end
+            % Manchester signaling
+            case 3
+                if bin2dArray(i,j) == 1
+                    final_values(counter) = A;
+                    counter = counter + 1;
+                    final_values(counter) = -1*A;
+                elseif bin2dArray(i,j) == 0
+                    final_values(counter) = -1*A;
+                    counter = counter + 1;
+                    final_values(counter) = A;
+                end
+            otherwise
+                disp('Invalid encoder type');
+        end
+        counter = counter + 1;
+    end
+end
+
+y = final_values;
