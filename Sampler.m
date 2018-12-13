d84668324759731400d3220725fee7373b0d24ed
muhammad20 <muhammadahmad201196@hotmail.com>
2018-12-13 22:54:24 +0200

    testing sampler

--- Sampler.m
+++ Sampler.m
@@ -1,10 +1,9 @@
-function x = Sampler(fm, fs)
+function x = Sampler(mp, fm, fs)
 %we let the arbitrary m(t) = 5 * cos(2 * pi * fm * t)
 t = 0:1/fs * 10 ^ -2:2 * 1/fm;
-m_t = 5 * cos(2 * pi * fm * t);
+m_t = mp * sin(2 * pi * fm * t);
 ts = 0:1/fs: 2 * 1/fm;
-samples = 5 * cos(2 * pi * fm * ts);
-%disp(ts);
+samples = mp * sin ( 2 * pi * fm * ts);
 plot(t,m_t);
 % to plot over the existing plot
 hold on;
