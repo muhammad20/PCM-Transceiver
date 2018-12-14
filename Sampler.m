19ea02fd96c114520721ac40f772197890555a60
muhammad20 <muhammadahmad201196@hotmail.com>
2018-12-14 13:50:29 +0200

    changed sampler to accept signal type as input

--- Sampler.m
+++ Sampler.m
@@ -1,12 +1,12 @@
-function x = Sampler(mp, fm, fs)
-%we let the arbitrary m(t) = 5 * cos(2 * pi * fm * t)
-t = 0:1/fs * 10 ^ -2:2 * 1/fm;
-m_t = mp * sin(2 * pi * fm * t);
+function x = Sampler(fm, fs, m_t, offset)
+%accept m(t) as an input to the function
+t = 0:1/fs * 1/offset : 2 * 1/fm;
 ts = 0:1/fs: 2 * 1/fm;
-samples = mp * sin ( 2 * pi * fm * ts);
-plot(t,m_t);
-% to plot over the existing plot
-hold on;
-%stem(ts,samples);
+samples = zeros(1, length(ts));
+k = 1;
+for i = 1 : offset : length(t)
+    samples(k) = m_t(i);
+    k = k + 1;
+end
 x = samples;
 end
--- main.m
+++ main.m
@@ -1,17 +1,36 @@
-mp = 3;
-L = 8;
-fs = 50;
-fm = 1;
+mp = 5;
+L = 16;
+fs = 100;
+fm = 5;
+num_cycles = 2;
+offset = 10;
 ts = 0:1/fs: 2 * 1/fm;
-t = 0:1/fs * 10 ^ -2:2 * 1/fm;
+t = 0:1/fs * 1/offset: 2 * 1/fm;
+
+% original signal
 m_t = mp * sin ( 2 * pi * fm * t);
-samples = Sampler(mp, fm, fs);
-stem(ts, samples);
-grid on;
+
+%sampled signal
+samples = Sampler(fm, fs, m_t, offset);
+
+%quantized signal
 quantized_samples = Quantizer(1, samples, L, mp);
-% disp(samples);
-% disp(quantized_samples);
-stem(ts,quantized_samples);
+
+%encoded signal to be transmitted
 encoded_msg = Encoder(1, quantized_samples, mp, L, fs);
+
+%decoded signal at receiver
 received_quantized = Decoder(1, encoded_msg,mp,L);
-stairs(ts,received_quantized);
+
+%plotting the data at transmitter
+subplot(2,1,1);
+plot(t, m_t);
+hold on;
+stem(ts, samples);
+subplot(2,1,2);
+plot(t,m_t);
+hold on;
+stairs(ts,quantized_samples);
+
+%plotting data at receiver
+%stairs(ts, received_quantized);
