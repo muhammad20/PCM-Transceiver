a1c07a83d10378a7d4a2e2d06159a0e60e4a586f
muhammad20 <muhammadahmad201196@hotmail.com>
2018-12-21 15:19:07 +0200

    added reconstruction fllter (under construction)

---
+++ ReconstructionFilter.m
@@ -0,0 +1,8 @@
+function x = ReconstructionFilter(received_quantized, fm, fs)
+    t_filter = -2 : 1/fs: 2;
+    
+    S=sinc(2*(fm+1)*t_filter);
+   
+    plot(t_filter,conv(S,received_quantized,'same'));
+    
+end
