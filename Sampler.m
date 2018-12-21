4af9fb84d54387d32182788a34e85a7cd9dc6a9f
muhammad20 <muhammadahmad201196@hotmail.com>
2018-12-21 15:20:04 +0200

    sampling the signal from -tm to tm (2 cycles)
    instead of sampling it from 0 to 2tm

--- Sampler.m
+++ Sampler.m
@@ -1,7 +1,7 @@
 function x = Sampler(fm, fs, m_t, offset)
 %accept m(t) as an input to the function
-t = 0:1/fs * 1/offset : 2 * 1/fm;
-ts = 0:1/fs: 2 * 1/fm;
+t = -1/fm:1/fs * 1/offset :1/fm;
+ts = -1/fm:1/fs: 1/fm;
 samples = zeros(1, length(ts));
 k = 1;
 for i = 1 : offset : length(t)
